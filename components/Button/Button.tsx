3c1ffc3528613676516487803792343a2095dbd6
lev <lev.domasnix2013@gmail.com>
2021-09-23 00:46:26 +0500

    HTMLProps

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,13 +3,14 @@ import styles from './Button.module.css'
 import {ButtonProps} from "./Button.props";
 import cn from 'classnames'
 
-export const Button: FC<ButtonProps> = ({children, appearance})=> {
+export const Button: FC<ButtonProps> = ({children, appearance, className, ...props})=> {
     return (
         <button
-            className={cn(styles.button, {
+            className={cn(styles.button, className, {
                 [styles.primary]: appearance == 'primary',
                 [styles.ghost]: appearance == 'ghost',
             })}
+            {...props}
         >
             {children}
         </button>
