f56767724f17dcf29216c2d6000785fd641008a2
lev <lev.domasnix2013@gmail.com>
2021-09-26 21:30:23 +0500

    Верстка footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import {FC} from "react";
 import {FooterProps} from "./Footer.props";
 import style from './Footer.module.css'
+import cn from "classnames";
+import { format } from 'date-fns'
 
-export const Footer: FC<FooterProps> = ({...props}) => {
+export const Footer: FC<FooterProps> = ({className, ...props}) => {
     return (
-        <div {...props} >
-            Footer
-        </div>
+        <footer {...props} className={cn(className, style.footer)}>
+            <div>
+                OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+            </div>
+            <a href='#' target='_blank'>Пользовательское соглашение</a>
+            <a href='#' target='_blank'>Политика конфиденциальности</a>
+        </footer>
     )
 }
