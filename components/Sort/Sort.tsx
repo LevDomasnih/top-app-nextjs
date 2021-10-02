184a6c2198ac82abb611b2071aa5a2df8b3f94dc
lev <lev.domasnix2013@gmail.com>
2021-10-02 21:59:38 +0500

    Компонент сортировки

---
+++ components/Sort/Sort.tsx
@@ -0,0 +1,31 @@
+import {FC} from "react";
+import {SortEnum, SortProps} from "./Sort.props";
+import SortIcon from './sort.svg'
+import cn from "classnames";
+import styles from './Sort.module.css'
+
+export const Sort: FC<SortProps> = ({className, sort, setSort, ...props}) => {
+    return (
+        <div
+            className={cn(styles.sort, className)}
+            {...props}
+        >
+            <span
+                onClick={() => setSort(SortEnum.Rating)}
+                className={cn({
+                    [styles.active]: sort === SortEnum.Rating
+                })}
+            >
+                <SortIcon className={styles.sortIcon} /> По рейтингу
+            </span>
+            <span
+                onClick={() => setSort(SortEnum.Price)}
+                className={cn({
+                    [styles.active]: sort === SortEnum.Price
+                })}
+            >
+                <SortIcon className={styles.sortIcon} /> По цене
+            </span>
+        </div>
+    )
+}
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,16 +1,17 @@
 import {FC} from "react";
 import {TopPageComponentProps} from "./TopPageComponent.props";
-import {Advantages, HhData, Htag, Paragraph, Tag} from "../../components";
+import {Advantages, HhData, Htag, Sort, Tag} from "../../components";
 import styles from "./TopPageComponent.module.css"
 import {TopLevelCategory} from "../../interfaces/page.interface";
+import {SortEnum} from "../../components/Sort/Sort.props";
 
-export const TopPageComponent: FC<TopPageComponentProps> = ({ firstCategory, page, products }) => {
+export const TopPageComponent: FC<TopPageComponentProps> = ({firstCategory, page, products}) => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.title}>
                 <Htag tag='h1'>{page.title}</Htag>
                 {products.length && <Tag color="green" size="m">{products.length}</Tag>}
-                <span>Сортировка</span>
+                <Sort sort={SortEnum.Rating} setSort={() => {return false}} />
             </div>
             <div>
                 {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
@@ -23,11 +24,11 @@ export const TopPageComponent: FC<TopPageComponentProps> = ({ firstCategory, pag
             {page.advantages && page.advantages.length > 0 && (
                 <>
                     <Htag tag='h2'>Преимущества</Htag>
-                    <Advantages advantages={page.advantages} />
+                    <Advantages advantages={page.advantages}/>
                 </>
             )}
             {page.seoText && (
-                <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }}/>
+                <div className={styles.seo} dangerouslySetInnerHTML={{__html: page.seoText}}/>
             )}
             <Htag tag='h2'>Получаемые навыки</Htag>
             {page.tags.map(t => <Tag color='primary' key={t}>{t}</Tag>)}
