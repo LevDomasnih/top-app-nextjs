dbd4b6ad19b846414019e5f74afd4aef4437f782
lev <lev.domasnix2013@gmail.com>
2021-10-02 11:24:24 +0500

    Верстка sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,14 @@ import {FC} from "react";
 import {SidebarProps} from "./Sidebar.props";
 import style from './Sidebar.module.css'
 import {Menu} from "../../Menu/Menu";
+import Logo from '../logo.svg'
+import cn from "classnames";
 
-export const Sidebar: FC<SidebarProps> = ({...props}) => {
+export const Sidebar: FC<SidebarProps> = ({className, ...props}) => {
     return (
-        <div {...props} >
+        <div className={cn(className, style.sidebar)} {...props}>
+            <Logo className={style.logo} />
+            <div>Поиск</div>
             <Menu />
         </div>
     )
