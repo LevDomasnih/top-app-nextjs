ec63d200d70d66d3c019f4879e730367438f1e9a
lev <lev.domasnix2013@gmail.com>
2021-10-02 15:45:46 +0500

    Regex отображения цены

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -3,6 +3,7 @@ import {HhDataProps} from "./HhData.props";
 import {Card} from "../Card/Card";
 import RateIcon from './rate.svg'
 import styles from './HhData.module.css'
+import {priceRu} from "../../helpers/helpers";
 
 export const HhData: FC<HhDataProps> = ({ count, juniorSalary, middleSalary, seniorSalary }) => {
     return (
@@ -14,7 +15,7 @@ export const HhData: FC<HhDataProps> = ({ count, juniorSalary, middleSalary, sen
             <Card className={styles.salary}>
                 <div>
                     <div className={styles.title}>Начальный</div>
-                    <div className={styles.salaryValue}>{juniorSalary}</div>
+                    <div className={styles.salaryValue}>{priceRu(juniorSalary)}</div>
                     <div className={styles.rate}>
                         <RateIcon className={styles.filled} />
                         <RateIcon />
@@ -23,7 +24,7 @@ export const HhData: FC<HhDataProps> = ({ count, juniorSalary, middleSalary, sen
                 </div>
                 <div>
                     <div className={styles.title}>Средний</div>
-                    <div className={styles.salaryValue}>{middleSalary}</div>
+                    <div className={styles.salaryValue}>{priceRu(middleSalary)}</div>
                     <div className={styles.rate}>
                         <RateIcon className={styles.filled} />
                         <RateIcon className={styles.filled} />
@@ -32,7 +33,7 @@ export const HhData: FC<HhDataProps> = ({ count, juniorSalary, middleSalary, sen
                 </div>
                 <div>
                     <div className={styles.title}>Профессионал</div>
-                    <div className={styles.salaryValue}>{seniorSalary}</div>
+                    <div className={styles.salaryValue}>{priceRu(seniorSalary)}</div>
                     <div className={styles.rate}>
                         <RateIcon className={styles.filled} />
                         <RateIcon className={styles.filled} />
