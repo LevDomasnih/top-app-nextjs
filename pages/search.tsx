c686b1b046a844ca5d2eb1d9eea523f29dbc1e26
lev <lev.domasnix2013@gmail.com>
2021-10-02 11:24:24 +0500

    Верстка sidebar

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,4 @@
 import {withLayout} from "../layout/Layout";
-import {GetStaticProps} from "next";
-import axios from "axios";
-import {MenuItem} from "../interfaces/menu.interface";
 
 const Search = () => {
     return (
@@ -12,16 +9,3 @@ const Search = () => {
 }
 
 export default withLayout(Search)
-
-// export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-//     const firstCategory = 0
-//     const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-//         firstCategory
-//     })
-//     return {
-//         props: {
-//             menu,
-//             firstCategory
-//         }
-//     }
-// }
