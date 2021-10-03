8d84b3d1d5be31e57b8e002fc21d683642afc3de
lev <lev.domasnix2013@gmail.com>
2021-10-03 14:15:14 +0500

    Компонент поиска

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -4,12 +4,13 @@ import style from './Sidebar.module.css'
 import {Menu} from "../../Menu/Menu";
 import Logo from '../logo.svg'
 import cn from "classnames";
+import {Search} from "../../components";
 
 export const Sidebar: FC<SidebarProps> = ({className, ...props}) => {
     return (
         <div className={cn(className, style.sidebar)} {...props}>
             <Logo className={style.logo} />
-            <div>Поиск</div>
+            <Search />
             <Menu />
         </div>
     )
