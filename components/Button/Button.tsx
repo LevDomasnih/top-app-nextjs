268063875718401576df6cbbd69c363c83e6a011
lev <lev.domasnix2013@gmail.com>
2021-10-13 22:49:34 +0500

    Анимация кнопок

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,10 +3,14 @@ import styles from './Button.module.css'
 import {ButtonProps} from "./Button.props";
 import ArrowIcon from './arrow.svg'
 import cn from 'classnames'
+import { motion } from "framer-motion";
 
 export const Button: FC<ButtonProps> = ({children, arrow = 'none', appearance, className, ...props})=> {
     return (
-        <button
+        <motion.button
+            whileHover={{
+                scale: 1.05
+            }}
             className={cn(styles.button, className, {
                 [styles.primary]: appearance == 'primary',
                 [styles.ghost]: appearance == 'ghost',
@@ -20,6 +24,6 @@ export const Button: FC<ButtonProps> = ({children, arrow = 'none', appearance, c
             })}>
                 <ArrowIcon />
             </span>}
-        </button>
+        </motion.button>
     )
 }
