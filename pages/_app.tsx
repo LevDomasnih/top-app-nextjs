42e9ecbb271548809991996b68dc2bd5a816257a
lev <lev.domasnix2013@gmail.com>
2021-10-15 00:33:11 +0500

    Добавление meta на страницу

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import {AppProps} from "next/dist/shared/lib/router/router";
 import Head from "next/head";
 
-function MyApp({Component, pageProps}: AppProps): JSX.Element {
+function MyApp({Component, pageProps, router}: AppProps): JSX.Element {
     return <>
         <Head>
             <title>Top-page</title>
@@ -12,6 +12,8 @@ function MyApp({Component, pageProps}: AppProps): JSX.Element {
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap"
                   rel="stylesheet"/>
+            <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+            <meta property='og:locale' content='ru_RU' />
         </Head>
         <Component {...pageProps} />
     </>
