0140f3704a61f48a531362ac9c987a4c946ca147
lev <lev.domasnix2013@gmail.com>
2021-10-15 01:36:57 +0500

    Кастомные 404 и 500 ошибки

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import {Htag} from "../components";
+
+function Error404() {
+    return (
+        <>
+            <Htag tag='h1'>Ошибка 404</Htag>
+        </>
+    )
+}
+
+export default Error404
